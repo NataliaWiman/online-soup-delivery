c7bf7f9ef244e0d499aa6274f46c394705c0c6f3
NataliaWiman <natalia.wiman@gmail.com>
2020-11-01 20:45:25 +0100

    trying a fix

--- src/components/Firebase/firebase.ts
+++ src/components/Firebase/firebase.ts
@@ -5,7 +5,7 @@ import 'firebase/auth';
 const config = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DB,
+  databaseURL: "https://online-soup-delivery.firebaseio.com",
   projectId: process.env.REACT_APP_PID,
   storageBucket: process.env.REACT_APP_SB,
   messagingSenderId: process.env.REACT_APP_SID,
